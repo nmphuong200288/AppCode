9f1c653f08adc8a5f941280b125e62569ed438eb
nmphuong200288 <88701458+nmphuong200288@users.noreply.github.com>
2024-03-12 15:32:09 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
                         fontWeight: "bolder",
                     }}
                 >
-                    TODO LIST OK
+                    TODI LIST
                 </Row>
   
                 <hr />
